78e55f6b3cbc4afe5e2a980e1f08d7544dcc5357
Pedro <phsr2001@gmail.com>
2020-11-14 22:40:11 -0300

    Create project and config profiles(proprerties)

---
+++ src/main/java/br/com/pyim/mgpyim/MgpyimApplication.java
@@ -0,0 +1,14 @@
+package br.com.pyim.mgpyim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgpyimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgpyimApplication.class, args);
+	}
+
+
+}
